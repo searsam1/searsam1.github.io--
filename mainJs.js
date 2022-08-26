8dc5530d9edb4ba6a5c4659238d7422d2d37a8ad
Alec <amsears60441@gmail.com>
2022-08-26 12:04:35 -0600

    added bolt to navbar

--- mainJs.js
+++ mainJs.js
@@ -1,12 +1,21 @@
 function show(id) {
 
-    const ele = document.getElementById(id)
+    const ele = document.getElementById(id);
 
     if (ele.style.display == "none"){
-        ele.style.display = "block"
+        ele.style.display = "block";
     }
     else{
-        ele.style.display = "none"
+        ele.style.display = "none";
     }
 
+    const bolt = document.getElementById("light-bolt");
+
+    if (bolt.style.color == "darkred"){
+        bolt.style.color = "black";    
+        
+    }
+    else {
+        bolt.style.color = "darkred";
+    }
   }
