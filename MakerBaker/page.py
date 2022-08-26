34ff0e05510a5031ddc4d916c081d9b0db7fe125
Alec <amsears60441@gmail.com>
2022-08-26 10:55:52 -0600

    added f string example

--- MakerBaker/page.py
+++ MakerBaker/page.py
@@ -1,8 +1,13 @@
 
-import math
-print('The value of e is approximately %5.5f.' % math.e)
-# The value of e is approximately 2.71828.
+# import math
+# print('The value of e is approximately %5.5f.' % math.e)
+# # The value of e is approximately 2.71828.
 
-message = "hello world!"
-print('Message : %s' % message)
-# Message : hello world!
+# message = "hello world!"
+# print('Message : %s' % message)
+# # Message : hello world!
+
+first_name, last_name = "john", "sullivan"
+full_name = f"{first_name.title()} {last_name.title()}"
+print(full_name)
+# john sullivan
