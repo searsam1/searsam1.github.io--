43f885194a6d0f24eb76d0acb908fd1e60a4a705
Alec <amsears60441@gmail.com>
2022-08-27 07:04:36 -0600

    added ascii table

--- MakerBaker/page.py
+++ MakerBaker/page.py
@@ -7,7 +7,20 @@
 # print('Message : %s' % message)
 # # Message : hello world!
 
-first_name, last_name = "john", "sullivan"
-full_name = f"{first_name.title()} {last_name.title()}"
-print(full_name)
-# john sullivan
+# first_name, last_name = "john", "sullivan"
+# full_name = f"{first_name.title()} {last_name.title()}"
+# print(full_name)
+# # john sullivan
+
+# import string
+import numpy as np
+
+prntable = r"""0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0000"""
+
+arr = [] 
+for i in prntable:
+    arr.append([str(ord(i)).rjust(3,'0'), i])
+
+arr = np.array(arr)
+arr = arr.reshape((14,14))
+print(arr)
